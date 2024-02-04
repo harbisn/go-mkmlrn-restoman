b50688b6df74c0827eede44f15b56eaca48b1674
harbisn <harbisn5@gmail.com>
2024-02-04 15:36:27 +0700

    add viper and  yaml file to set dsn when connect to db, add .gitignore for config.yaml

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -1,8 +1,11 @@
 package database
 
 import (
+	"github.com/spf13/viper"
 	psql "gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"strings"
 )
 
 var (
@@ -10,7 +13,7 @@ var (
 )
 
 func Connect() {
-	dsn := "host=your_host user=your_user password=your_password dbname=your_dbname port=your_port sslmode=disable"
+	dsn := viper.GetString("database.dsn")
 	d, err := gorm.Open(psql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -20,4 +23,29 @@ func Connect() {
 
 func GetDB() *gorm.DB {
 	return db
+}
+
+func loadConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %s", err)
+	}
+
+	viper.SetEnvPrefix("GO_MKMLRN_RESTOMAN")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+}
+
+func init() {
+	loadConfig()
+	requiredFields := []string{"database.dsn"}
+	for _, field := range requiredFields {
+		if !viper.IsSet(field) {
+			log.Fatalf("Missing required configuration field: %s", field)
+			return
+		}
+	}
 }
